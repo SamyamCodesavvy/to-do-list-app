b290aaac69cb3cb5594cae8a53b28a6d67f9a456
Samyam Giri <123580078+SamyamCodesavvy@users.noreply.github.com>
2023-11-18 22:46:26 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
 import sqlite3 as sql
-bg_color1 = "#677580"
+color1 = "#677580"
 font_name1 = "Arial"
+font_name2 = "Times New Roman"
 root = Tk()
 root.title("To-Do List App")
 root.iconbitmap("to_do_list_icon.ico")
@@ -19,8 +20,11 @@ tasks = []
 # task_label.place(x=5, y=5)
 topbar_image = PhotoImage(file="topbar.png")
 Label(root, image=topbar_image).pack()
-Label(root, text="ALL TASKS", font=(font_name1, 12 ,"bold"), bg=bg_color1, fg= "white").place(x=80, y=20)
-
-
+head_text = Label(root, text="TO-DO LIST", font=(font_name1, 16 ,"bold"), bg=color1, fg= "white")
+head_text.place(x=141, y=26)
+task_here = Label(root, text="Enter task here:", font=(font_name2, 14, "italic", "bold"), padx=10, pady=5)
+task_here.place(y=84)
+task_entry = Entry(root, font = (font_name1, 12), width=26)
+task_entry.place(x=143, y=84, height=31)
 
 root.mainloop()
