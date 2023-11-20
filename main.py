5507de1fe93d623a52d6c0b14af7bc048e46d457
Samyam Giri <123580078+SamyamCodesavvy@users.noreply.github.com>
2023-11-20 12:10:36 +0545

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ color4 = "#000000"
 color5 = "#FFFFFF"
 font_name1 = "Arial"
 font_name2 = "Times New Roman"
+font_name3 = "Comic Sans MS"
 def set_cursor_position(event):
     task_entry.icursor(5)
 root = Tk()
 root.title("To-Do List App")
 root.iconbitmap("to_do_list_icon.ico")
 root.resizable(False,False)
-root.geometry("400x550+400+80")
+root.geometry("410x530+400+80")
 root.config()
 db_connect = sql.connect("TodoTaskLists.db")
 cursor = db_connect.cursor()
@@ -24,9 +25,9 @@ topbar_image = PhotoImage(file="topbar.png")
 Label(root, image=topbar_image).pack()
 head_text = Label(root, text="TO-DO LIST", font=(font_name1, 16 ,"bold"), background=color1, foreground=color5)
 head_text.place(x=141, y=26)
-task_here = Label(root, text="Enter task:", font=(font_name2, 14, "italic", "bold"), padx=7, pady=5)
-task_here.place(y=105)
-task_entry = Entry(root, font=(font_name1, 12, "bold"), width=25, background=color2, foreground=color5)
+task_here = Label(root, text="Enter task:", font=(font_name3, 13), padx=7, pady=5)
+task_here.place(y=102)
+task_entry = Entry(root, font=(font_name1, 12, "italic", "bold"), width=26, background=color2, foreground=color5)
 task_entry.place(x=107, y=105, height=33)
 task_entry.focus()
 add_button = Button(
@@ -37,11 +38,41 @@ add_button = Button(
     background=color4,
     foreground=color5,
     activebackground=color3,
-    activeforeground=color4)
-add_button.place(x=337, y=105)
+    activeforeground=color4,
+    cursor="hand2")
+add_button.place(x=347, y=105)
 delete_button = Button(
     root,
-    text="Delete",
-
+    text="DELETE THIS TASK",
+    font=(font_name1, 12, "bold"),
+    width = 17,
+    background=color4,
+    foreground=color5,
+    activebackground=color3,
+    activeforeground=color4,
+    cursor="hand2",
+    padx=5
 )
+delete_button.place(x=12, y=150)
+delete_all_button = Button(
+    root,
+    text="DELETE ALL TASKS",
+    font=(font_name1, 12, "bold"),
+    width = 17,
+    background=color4,
+    foreground=color5,
+    activebackground=color3,
+    activeforeground=color4,
+    cursor="hand2",
+    padx=5
+)
+delete_all_button.place(x=209, y=150)
+frame = Frame(root, bd=3, width=700, height=280, background=color2)
+frame.pack(pady=(115,0))
+listbox = Listbox(frame, font=(font_name2, 10), width=62, height=20, background=color2, foreground=color4)
+listbox.pack(side=LEFT, fill=BOTH, padx=2)
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side=RIGHT, fill=BOTH)
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
 root.mainloop()
